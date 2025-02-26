ba0b7eb5eac46500b11847f36159ff8dca580634
nouserdata <govindkumary2066@gmail.com>
2025-02-26 14:06:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 BOT_NAME = "Moni Uploader 2"
-API_ID    = 24495656
-API_HASH  = "61afcf68c6429714dd18acd07f246571"
-BOT_TOKEN = "8040014845:AAHAo3zgpikp-4j2PLQofMxSwtoOdQKTQLg"
+API_ID    = 18116358
+API_HASH  = "80e7597f27a57df271dfc500120d4ea9"
+BOT_TOKEN = "8032649391:AAFqxv4GtAnGar2Md1MoPaOTzNgDlneX1Uw"
 MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
